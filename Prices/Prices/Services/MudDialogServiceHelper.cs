5a32d52379aa1b0439b5f1f4449e8e19e76504d6
tetr4lab. <48040768+tetr4lab@users.noreply.github.com>
2024-09-08 00:43:45 +0900

    不要部分の削除(NewItem.razor -> ItemListFooter.razor) ドキュメントに現状を反映

--- Prices/Prices/Services/MudDialogServiceHelper.cs
+++ Prices/Prices/Services/MudDialogServiceHelper.cs
@@ -7,26 +7,6 @@ namespace Prices.Services;
 
 public static class MudDialogServiceHelper {
 
-    /// <summary>アイテム用ダイアログを開く</summary>
-    public static async Task<IDialogReference> OpenItemDialog<T1, T2> (this IDialogService service, T1 item, Func<T2, Task>? changed = null, Action? updated = null) {
-        var options = new DialogOptions { MaxWidth = MaxWidth.ExtraLarge, FullWidth = true, };
-        var parameters = new DialogParameters { };
-        if (changed?.Target != null) {
-            parameters.Add ("OnChangeDialog", EventCallback.Factory.Create (changed.Target, changed));
-        }
-        if (updated?.Target != null) {
-            parameters.Add ("OnStateHasChanged", EventCallback.Factory.Create (updated.Target, updated));
-        }
-        if (item is Book book) {
-            parameters.Add ("Item", book);
-            return await service.ShowAsync<BookDialog> ($"{Book.TableLabel}詳細", parameters, options);
-        } else if (item is Author author) {
-            parameters.Add ("Item", author);
-            return await service.ShowAsync<AuthorDialog> ($"{Author.TableLabel}詳細", parameters, options);
-        }
-        throw new ArgumentNullException (nameof (item));
-    }
-
     /// <summary>リロードの確認</summary>
     public static async Task<DialogResult?> ReloadConfirmation (this IDialogService dialogService, IEnumerable<string?> message, string cancellationLabel = "")
         => await Confirmation (dialogService, message, title: "リロードの確認", acceptionLabel: "Reload", acceptionColor: Color.Success, cancellationLabel: cancellationLabel);
--- Prices/Prices/Services/PricesDataSet.cs
+++ Prices/Prices/Services/PricesDataSet.cs
@@ -93,9 +93,6 @@ public sealed class PricesDataSet {
             typeof (T) == typeof (Product) ? Products as List<T> :
             typeof (T) == typeof (Store) ? Stores as List<T> :
             typeof (T) == typeof (Price) ? Prices as List<T> :
-
-            typeof (T) == typeof(Author) ? Authors as List<T> :
-            typeof(T) == typeof(Book) ? Books as List<T> :
             null
         ) ?? new();
 
@@ -111,20 +108,6 @@ public sealed class PricesDataSet {
     /// <summary>ロード済みのモデルインスタンス</summary>
     public List<Price> Prices { get; private set; } = new ();
 
-    /// <summary>ロード済みのモデルインスタンス</summary>
-    public List<Author> Authors {
-        get => _authors;
-        set => (_authors = value).ForEach(author => author.DataSet = this);
-    }
-    private List<Author> _authors = new();
-
-    /// <summary>ロード済みのモデルインスタンス</summary>
-    public List<Book> Books {
-        get => _books;
-        set => (_books = value).ForEach(book => book.DataSet = this);
-    }
-    private List<Book> _books = new();
-
     /// <summary>有効性の検証</summary>
     public bool Valid
         => IsReady
@@ -132,9 +115,6 @@ public sealed class PricesDataSet {
         && Products != null && !Products.Exists (i => i.Id <= 0)
         && Stores != null && !Stores.Exists (i => i.Id <= 0)
         && Prices != null && !Prices.Exists (i => i.Id <= 0)
-
-        && _authors != null && !_authors.Exists (i => i.DataSet != this || i.Id <= 0)
-        && _books != null && !_books.Exists (i => i.DataSet != this || i.Id <= 0)
         ;
 
     /// <summary>一覧セットをアトミックに取得</summary>
@@ -223,42 +203,6 @@ public sealed class PricesDataSet {
         GetAll<T> ().Remove (item);
         return result;
     }
-
-    /// <summary>読み込み済み総リストから対象Idの存在を確認</summary>
-    public bool ExistsById<T1, T2> (long id)
-        where T1 : PricesBaseModel<T1, T2>, new ()
-        where T2 : PricesBaseModel<T2, T1>, new()
-        => GetAll<T1> ()?.Exists (i => i.Id == id) == true;
-
-    /// <summary>読み込み済み総リストから対象アイテムを得る</summary>
-    public T1? GetItemById<T1, T2> (T1 item)
-        where T1 : PricesBaseModel<T1, T2>, new()
-        where T2 : PricesBaseModel<T2, T1>, new()
-        => GetAll<T1> ()?.Find (i => i.Id == item.Id);
-
-    /// <summary>読み込み済み総リストから対象と同名のアイテムを得る</summary>
-    public T1? GetOtherItemByName<T1, T2> (T1 item)
-        where T1 : PricesBaseModel<T1, T2>, new()
-        where T2 : PricesBaseModel<T2, T1>, new()
-        => GetAll<T1> ()?.Find (i => i.UniqueKey.Equals (item.UniqueKey) && i.Id != item.Id);
-
-    /// <summary>読み込み済み総リストから対象と同名のアイテムを得る</summary>
-    public T1? GetItemByName<T1, T2> (string name)
-        where T1 : PricesBaseModel<T1, T2>, new()
-        where T2 : PricesBaseModel<T2, T1>, new()
-        => GetAll<T1> ()?.Find (i => i.RowLabel == name);
-
-    /// <summary>読み込み済み総リストから対象と同名の別存在を確認</summary>
-    public bool ExistsOtherByName<T1, T2> (T1 item)
-        where T1 : PricesBaseModel<T1, T2>, new()
-        where T2 : PricesBaseModel<T2, T1>, new()
-        => GetOtherItemByName<T1, T2> (item) != null;
-
-    /// <summary>読み込み済み総リストから対象と同名の存在を確認</summary>
-    public bool ExistsByName<T1, T2> (string name)
-        where T1 : PricesBaseModel<T1, T2>, new()
-        where T2 : PricesBaseModel<T2, T1>, new()
-        => GetItemByName<T1, T2> (name) != null;
 
     /// <summary>SQLで使用するテーブル名またはカラム名を得る</summary>
     /// <param name="name">プロパティ名</param>
@@ -405,169 +349,6 @@ public sealed class PricesDataSet {
         }
     }
 
-    /// <summary>一覧を取得</summary>
-    private async Task<Result<List<T1>>> GetListAsync<T1, T2> ()
-        where T1 : PricesBaseModel<T1, T2>, IPricesModel, new()
-        where T2 :  PricesBaseModel<T2, T1>, IPricesModel, new() {
-        var table = GetSqlName<T1> ();
-        return await ProcessAndCommitAsync (async () => {
-            return await database.FetchAsync<T1> (
-                $@"select {table}.*, group_concat({GetSqlName<T2> ()}Id) as _relatedIds
-                from {table}
-                left join AuthorBook on {table}.Id = AuthorBook.{table}Id
-                group by {table}.Id;"
-            );
-        });
-    }
-
-    /// <summary>一覧ペアをアトミックに取得</summary>
-    public async Task<(Result<List<T1>> books, Result<List<T2>> authors)> GetPairAsync<T1, T2> ()
-        where T1 : PricesBaseModel<T1, T2>, IPricesModel, new()
-        where T2 : PricesBaseModel<T2, T1>, IPricesModel, new() {
-        var result = await ProcessAndCommitAsync (async () => {
-            var list1 = await GetListAsync<T1, T2> ();
-            var list2 = await GetListAsync<T2, T1> ();
-            return (list1, list2);
-        });
-        return result.ValueOrThrow;
-    }
-
-    /// <summary>単一アイテムを取得 (Idで特定) 【注意】総リストとは別オブジェクトになる</summary>
-    public async Task<Result<T1?>> GetItemByIdAsync<T1, T2> (T1 item)
-        where T1 : PricesBaseModel<T1, T2>, new()
-        where T2 : PricesBaseModel<T2, T1>, new() {
-        var table = GetSqlName<T1> ();
-        return await ProcessAndCommitAsync (async () => (await database.FetchAsync<T1?> (
-            $@"select {table}.*, Group_concat({GetSqlName<T2> ()}Id) as _relatedIds
-            from {table}
-            left join AuthorBook on {table}.Id = AuthorBook.{table}Id
-            where {table}.Id = @Id
-            group by {table}.Id;",
-            item
-        )).Single ());
-    }
-
-    /// <summary>単一アイテムを取得 (ユニークキーで特定) 【注意】総リストとは別オブジェクトになる</summary>
-    public async Task<Result<T1?>> GetItemByNameAsync<T1, T2> (T1 target)
-        where T1 : PricesBaseModel<T1, T2>, IPricesModel, new()
-        where T2 : PricesBaseModel<T2, T1>, IPricesModel, new() {
-        var table = GetSqlName<T1> ();
-        return await ProcessAndCommitAsync (async () => {
-            var result = await database.FetchAsync<T1?> (
-                $@"select {table}.*, Group_concat({GetSqlName<T2> ()}Id) as _relatedIds
-                from {table}
-                left join AuthorBook on {table}.Id = AuthorBook.{table}Id
-                where {T1.UniqueKeysSql}
-                group by {table}.Id;",
-                target
-            );
-            if (result.Count == 1) {
-                return result [0];
-            } else if (result.Count <= 0) {
-                throw Status.MissingEntry.GetException ();
-            } else {
-                throw Status.DuplicateEntry.GetException ();
-            }
-        });
-    }
-
-    /// <summary>アイテムの更新サブ処理 トランザクション内専用</summary>
-    private async Task<int> UpdateItemAsync<T1, T2> (T1 item)
-        where T1 : PricesBaseModel<T1, T2>, new()
-        where T2 : PricesBaseModel<T2, T1>, new() {
-        var table = GetSqlName<T1> ();
-        return await database.ExecuteAsync (
-            @$"update {table} set {GetSettingSql<T1> ()} where {table}.Id = @Id",
-            item
-        );
-    }
-
-    /// <summary>関係リンク追加サブ処理 トランザクション内専用</summary>
-    private async Task<int> AddRelationsAsync<T1, T2> (T1 item)
-        where T1 : PricesBaseModel<T1, T2>, new()
-        where T2 : PricesBaseModel<T2, T1>, new() {
-        if (item.RelatedIds.Count <= 0) { return 0; }
-        var valuesSqls = new List<string> ();
-        var parameters = new Dictionary<string, object> { { "Id1", item.Id } };
-        for (int i = 0; i < item.RelatedIds.Count; i++) {
-            valuesSqls.Add ($"(@Id1, @Id2_{i})");
-            parameters.Add ($"Id2_{i}", item.RelatedIds [i]);
-        }
-        return await database.ExecuteAsync (
-            $"insert into AuthorBook ({GetSqlName<T1> ()}Id, {GetSqlName<T2> ()}Id) values {string.Join (',', valuesSqls)};",
-            parameters
-        );
-    }
-
-    /// <summary>関係リンク削除サブ処理 トランザクション内専用</summary>
-    private async Task<int> RemoveRelationsAsync<T1, T2> (T1 item)
-        where T1 : PricesBaseModel<T1, T2>, new()
-        where T2 : PricesBaseModel<T2, T1>, new() {
-        return await database.ExecuteAsync (
-            $"delete from AuthorBook where {GetSqlName<T1> ()}Id = @Id",
-            item
-        );
-    }
-
-    /// <summary>アイテムの更新</summary>
-    public async Task<Result<int>> UpdateAsync<T1, T2> (T1 item)
-        where T1 : PricesBaseModel<T1, T2>, new()
-        where T2 : PricesBaseModel<T2, T1>, new() {
-        var result = await ProcessAndCommitAsync (async () => {
-            await RemoveRelationsAsync<T1, T2> (item);
-            await AddRelationsAsync<T1, T2> (item);
-            item.Version++;
-            return await UpdateItemAsync<T1, T2> (item);
-        });
-        if (result.IsSuccess && result.Value <= 0) {
-            result.Status = Status.MissingEntry;
-        }
-        return result;
-    }
-
-    /// <summary>単一アイテムの追加</summary>
-    public async Task<Result<T1>> AddAsync<T1, T2> (T1 item)
-        where T1 : PricesBaseModel<T1, T2>, new()
-        where T2 : PricesBaseModel<T2, T1>, new() {
-        var result = await ProcessAndCommitAsync (async () => {
-            item.Id = await database.ExecuteScalarAsync<int> (
-                @$"insert into {GetSqlName<T1> ()} ({GetColumnsSql<T1> ()}) values ({GetValuesSql<T1> ()});
-                select LAST_INSERT_ID ();",
-                item
-            );
-            if (item.Id > 0) {
-                await AddRelationsAsync<T1, T2> (item);
-                return 1;
-            }
-            return 0;
-        });
-        if (result.IsSuccess && result.Value <= 0) {
-            result.Status = Status.MissingEntry;
-        }
-        if (result.IsFailure) {
-            item.Id = default;
-        } else {
-            // ロード済みに追加
-            GetAll<T1> ().Add (item);
-        }
-        return new (result.Status, item);
-    }
-
-    /// <summary>一括アイテムの追加 (個別エラーを許容)</summary>
-    public async Task<Result<int>> AddAsync<T1, T2> (IEnumerable<T1> items)
-        where T1 : PricesBaseModel<T1, T2>, new()
-        where T2 : PricesBaseModel<T2, T1>, new() {
-        var results = new List<Result<T1>> ();
-        foreach (var item in items) {
-            var result = await AddAsync<T1, T2> (item);
-            results.Add (result);
-            if (result.IsFatal) {
-                throw result.Exception; // タイムアウトしたら以降は中断
-            }
-        }
-        return new (results.FirstFailedState (), results.FindAll (r => r.IsSuccess).Count);
-    }
-
     /// <summary>テーブルの次の自動更新値を得る</summary>
     /// <remarks>MySQL/MariaDBに依存</remarks>
     public async Task<long> GetAutoIncremantValueAsync<T> () where T : class {
@@ -604,140 +385,6 @@ public sealed class PricesDataSet {
             throw new NotSupportedException ("Failed to get auto_increment value.");
         }
         return Id;
-    }
-
-    /// <summary>一括アイテムの追加</summary>
-    public async Task<Result<int>> AddRangeAsync<T1, T2> (IEnumerable<T1> items)
-        where T1 : PricesBaseModel<T1, T2>, new()
-        where T2 : PricesBaseModel<T2, T1>, new() {
-        if (items.Count () <= 0) { return new Result<int> (Status.MissingEntry, 0); }
-        var result = await ProcessAndCommitAsync (async () => {
-            // 開始Idを取得
-            var Id = await GetAutoIncremantValueAsync<T1> ();
-            if (Id <= 0) {
-                // 開始Idの取得に失敗
-                throw new NotSupportedException ("Failed to get auto_increment value.");
-            }
-            // 主アイテムを挿入
-            var valuesSqls = new List<string> ();
-            for (int i = 0; i < items.Count (); i++) {
-                valuesSqls.Add ($"({GetValuesSql<T1> (i)})");
-            }
-            var result = await database.ExecuteAsync (
-                $"insert into {GetSqlName<T1> ()} ({GetColumnsSql<T1> ()}) values {string.Join (',', valuesSqls)};",
-                GetParamDictionary (items)
-            );
-            // 関係アイテムを挿入
-            valuesSqls = new List<string> ();
-            var parameters = new Dictionary<string, object> ();
-            foreach (var item in items) {
-                item.Id = Id++;
-                parameters.Add ($"Id1_{item.Id}", item.Id);
-                for (int i = 0; i < item.RelatedIds.Count; i++) {
-                    valuesSqls.Add ($"(@Id1_{item.Id}, @Id2_{item.Id}_{i})");
-                    parameters.Add ($"Id2_{item.Id}_{i}", item.RelatedIds [i]);
-                }
-            }
-            if (valuesSqls.Count > 0) {
-                var count = await database.ExecuteAsync (
-                    $"insert into AuthorBook ({GetSqlName<T1> ()}Id, {GetSqlName<T2> ()}Id) values {string.Join (',', valuesSqls)};",
-                    parameters
-                );
-                if (count < valuesSqls.Count) {
-                    System.Diagnostics.Debug.WriteLine ($"AddRangeAsync: ERROR: insert relations ({count}/{valuesSqls.Count})");
-                }
-            }
-            return result;
-        });
-        // ロード済みに追加
-        GetAll<T1> ().AddRange (items);
-        return result;
-    }
-
-    /// <summary>単一アイテムの削除</summary>
-    public async Task<Result<int>> RemoveAsync<T1, T2> (T1 item)
-        where T1 : PricesBaseModel<T1, T2>, new()
-        where T2 : PricesBaseModel<T2, T1>, new() {
-        var result = await ProcessAndCommitAsync (async () => {
-            var original = await GetItemByIdAsync<T1, T2> (item);
-            if (original.IsSuccess && original.Value != null) {
-                if (item.Version == original.Value.Version) {
-                    // 関係先は制約によって自動的に削除される
-                    return await database.ExecuteAsync (
-                        $"delete from {GetSqlName<T1> ()} where Id = @Id",
-                        item
-                    );
-                } else {
-                    throw new MyDataSetException ($"Version mismatch between {item.Version} and {original.Value.Version}");
-                }
-            }
-            return 0;
-        });
-        if (result.IsSuccess && result.Value <= 0) {
-            result.Status = Status.MissingEntry;
-        }
-        // ロード済みから除去
-        GetAll<T1> ().Remove (item);
-        return result;
-    }
-
-    /// <summary>バージョン照合を行うためのモデル</summary>
-    private class IdVersion {
-        [Column] public long Id { get; set; }
-        [Column] public int Version { get; set; }
-    }
-
-    /// <summary>一括アイテムの削除</summary>
-    public async Task<Result<int>> RemoveRangeAsync<T1, T2> (IEnumerable<T1> items)
-        where T1 : PricesBaseModel<T1, T2>, new()
-        where T2 : PricesBaseModel<T2, T1>, new() {
-        var table = GetSqlName<T1> ();
-        var status = Status.Unknown;
-        var result = await ProcessAndCommitAsync (async () => {
-            // 要求
-            var list = items.ToList ().ConvertAll (i => new IdVersion { Id = i.Id, Version = i.Version, });
-            // 既存
-            var ivSql = new List<string> ();
-            for (var i = 0; i < list.Count; i++) {
-                ivSql.Add ($"(@Id_{i}, @Version_{i})");
-            }
-            var entries = await database.FetchAsync<IdVersion> (
-                $"select * from {table} where (Id, Version) in ({string.Join (',', ivSql)})",
-                GetParamDictionary (list, withId: true)
-            );
-            // 一致
-            var targets = entries.ConvertAll (i => i.Id);
-            // 実行と結果
-            status = targets.Count < entries.Count () ? Status.VersionMismatch : (entries.Count () < list.Count ? Status.MissingEntry : Status.Success);
-            return targets.Count > 0 ? await database.ExecuteAsync (
-                $"delete from {table} where Id in (@Ids)",
-                new { Ids = targets }
-            ) : 0;
-        });
-        // ロード済みから除去
-        var allItems = GetAll<T1> ();
-        foreach (var item in items) {
-            allItems.Remove (item);
-        }
-        return new (status, result.Value);
-    }
-
-    /// <summary>テーブルが空であればオートインクリメントを初期化する</summary>
-    public async Task<Result<int>> ResetAutoIncrementAsync<T1, T2> ()
-        where T1 : PricesBaseModel<T1, T2>, new()
-        where T2 : PricesBaseModel<T2, T1>, new() {
-        var table1 = GetSqlName<T1> ();
-        return await ProcessAndCommitAsync (async () => {
-            var result = await database.ExecuteScalarAsync<int> (
-                @$"set @@empty := (select count(*) = 0 from {table1});
-                set @@sql := if(@@empty, 'alter table {table1} auto_increment = 1', 'select ""');
-                prepare st from @@sql;
-                execute st;
-                deallocate prepare st;
-                select @@empty;"
-            );
-            return result;
-        }); // `ALTER`を使用するので`SAVEPOINT`は使用不可
     }
 
 }
