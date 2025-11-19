9db8e714b3d6677862a1379d604c4af7ed19ef35
tetr4lab. <48040768+tetr4lab@users.noreply.github.com>
2025-11-19 15:30:57 +0900

    .NET 10, MudBlazor 8.14.0, MySqlConnector 2.5.0

--- Prices/Prices/Program.cs
+++ Prices/Prices/Program.cs
@@ -78,9 +78,10 @@ app.UseRequestLocalization (new RequestLocalizationOptions ()
 Thread.CurrentThread.CurrentCulture = new CultureInfo ("ja-JP");
 
 // Application Base Path
-var basePath = builder.Configuration ["AppBasePath"];
+var basePath = builder.Configuration ["AppBasePath"]?.TrimEnd ('/');
 if (!string.IsNullOrEmpty (basePath)) {
     app.UsePathBase (basePath);
+    app.MapBlazorHub ($"{basePath}/_blazor");
 }
 
 // Configure the HTTP request pipeline.
@@ -94,11 +95,11 @@ if (app.Environment.IsDevelopment ()) {
 
 app.UseHttpsRedirection ();
 
-app.UseStaticFiles ();
 app.UseAntiforgery ();
 app.UseAuthentication ();
 app.UseAuthorization ();
 
+app.MapStaticAssets ();
 app.MapRazorComponents<App> ()
     .AddInteractiveServerRenderMode ()
     .AddInteractiveWebAssemblyRenderMode ()
